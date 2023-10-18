b866c7856be47af7fb35179020ec71d89d07f21c
Vincent <33740652+wuchanghui5220@users.noreply.github.com>
2023-10-18 11:14:26 +0800

    Update and rename helper to fastinstallplaywright.sh

---
+++ fastinstallplaywright.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install python3 and pip playwright
+sudo apt update
+sudo apt install -y python3-pip nodejs npm
+sudo npm install -g playwright
+sudo pip install --break-system-packages playwright tqdm
+
+# install networkx matplotlib
+sudo pip install --user networkx matplotlib --break-system-packages
+
+# Use admin to install following packages if you want to run the Script for user admin 
+playwright install
+sudo playwright install-deps
