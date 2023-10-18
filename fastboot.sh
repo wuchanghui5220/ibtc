8e381be1efc090586f12b0db7177cff0c5c7305d
Vincent <33740652+wuchanghui5220@users.noreply.github.com>
2023-10-18 09:35:25 +0800

    Update fastboot.sh

--- fastboot.sh
+++ fastboot.sh
@@ -14,12 +14,19 @@ bash get-docker.sh
 # pull and run ipsec
 docker run --name ipsec-vpn-server --restart=always -v ikev2-vpn-data:/etc/ipsec.d -v /lib/modules:/lib/modules:ro -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server
 # download vpnclient config 
+echo "waiting for 5 second!"
+sleep 5
+echo "Copy vpnclient.mobileconfig "
 docker cp ipsec-vpn-server:/etc/ipsec.d/vpnclient.mobileconfig ./
+echo "Copy vpnclient.sswan "
 docker cp ipsec-vpn-server:/etc/ipsec.d/vpnclient.sswan ./
 
+echo "Set file owner and copy to /home/admin/"
 chown -R admin:admin  vpnclient.* 
 cp ./vpnclient.* /home/admin/
-
+echo "# ls -lh /home/admin/vpnclient.*"
 ls -lh /home/admin/vpnclient.*
 
+echo ""
 echo "Done! Enjoy!"
+echo ""
